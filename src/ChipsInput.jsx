44c074284067e5a4294a7290a044ff4ab72c115f
mqkhan125 <muhammadqasimkhan341@gmail.com>
2026-02-05 12:23:32 +0500

    Add CSS and JSX for ChipsInput component

--- src/ChipsInput.jsx
+++ src/ChipsInput.jsx
@@ -1,15 +1,31 @@
+import React, { useState } from 'react'
+
 const ChipsInput = () => {
+
+   const [input, setInput] = useState('')
+   const [data, setData] = useState([])
+
+   const handleInput = (event) => {
+     setInput(event.target.value)
+   }
+
+   const ChipsInput = (event) => {
+    if(event.key === "Enter" && input !== ""){
+        setData([...data, input])
+        setInput('')
+    }
+   }
 
   return (
     <>
     <div className="container">
         <div className="input-container">
             <h1>Chips Input</h1>
-            <input type="text" placeholder='Add Chips' />
+            <input type="text" placeholder='Add Chips' value={input} onChange={handleInput} onKeyUp={ChipsInput} />
         </div>
 
         <div className="chips-container">
-
+        
         </div>
     </div>
     </>
