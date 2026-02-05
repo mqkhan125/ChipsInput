d156ad0ac7494cbf08fd75f37e4ef85bee8c04f7
mqkhan125 <muhammadqasimkhan341@gmail.com>
2026-02-05 11:30:04 +0500

    Initial commit: chipsInput setup

---
+++ src/ChipsInput.jsx
@@ -0,0 +1,19 @@
+const ChipsInput = () => {
+
+  return (
+    <>
+    <div className="container">
+        <div className="input-container">
+            <h1>Chips Input</h1>
+            <input type="text" placeholder='Add Chips' />
+        </div>
+
+        <div className="chips-container">
+
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default ChipsInput
